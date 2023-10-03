1b98322111e357b75f6826e38f7d202a1513cdd0
JunqingJ <78386273+JunqingJ@users.noreply.github.com>
2023-10-03 10:31:20 +0100

    Delete Software/MATLAB/MATLAB code directory

--- Software/MATLAB/MATLAB code/Sinusoidal_input.m
+++
@@ -1,9 +0,0 @@
-CenterV=-0.02;
-d = daqlist("ni");
-deviceInfo = d{1, "DeviceInfo"};
-dq = daq("ni");
-dq.Rate = 2000;
-addoutput(dq, "Dev2", "ao0", "Voltage");
-outputSignal1 =centerV+0.5*sin((1:5000)*2*pi/5000);
-start(dq,'repeatoutput');
-write(dq,outputSignal1');
